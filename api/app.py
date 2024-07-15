845780ff660f5160f27c3c53e3edf194027165fb
Leonardo Luiz Gava <llgavamt@gmail.com>
2024-07-15 10:02:03 -0400

    add: host and port

--- api/app.py
+++ api/app.py
@@ -32,5 +32,5 @@ if __name__ == '__main__':
     if (app.config['DEBUG']):
         print(f' * Use example: http://127.0.0.1:5000/8073FF/D4FF7A')
         
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
     
